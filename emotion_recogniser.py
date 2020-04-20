58e8ef07f1ba95c0a8fa8060bc53854cd73acdc0
rohanblueboybaijal <rohanbaijal2702@gmail.com>
2020-04-21 02:44:19 +0530

    sentimental meow

---
+++ emotion_recogniser.py
@@ -0,0 +1,64 @@
+import numpy as np
+import cv2
+from detectfaces import get_faces
+from keras.models import load_model
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, BatchNormalization
+from keras.layers import Conv2D, MaxPooling2D, Activation
+from keras import backend as K
+import matplotlib.pyplot as plt
+
+img_rows, img_cols = 48, 48
+# 0=Angry, 1=Disgust, 2=Fear, 3=Happy, 4=Sad, 5=Surprise, 6=Neutral
+emotion = ['Angry', "Disgust", 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+model = []
+print('Loading Models...')
+print('0/3')
+for i in range(2):
+    m = load_model('saved_model/' + 'cnn'+str(i)+'.h5')
+    print(str(i+1) + '/3')
+    model.append(m)
+
+m = load_model('saved_model/ensemble.h5')
+model.append(m)
+print('3/3')
+
+print("Loading Complete!")
+
+def predict(x):
+    x_rev = np.flip(x, 1)
+    x = x.astype('float32')
+    x_rev = x_rev.astype('float32')
+    x /= 255
+    x_rev /= 255
+    p = np.zeros((1, 14))
+    p[:,0:7] = model[0].predict(x.reshape(1,48,48,1))
+    p[:,7:14] = model[1].predict(x_rev.reshape(1,48,48,1))
+    pre = model[2].predict(p)
+    return pre
+
+cap = cv2.VideoCapture(0)
+if not cap.isOpened():
+    cap.open()
+
+while(True):
+    ret, img = cap.read()
+
+    #faces = get_faces(img, method='dnn')
+    faces = get_faces(img, method='haar')
+
+    for i, (y,x,face) in enumerate(faces):
+        pre = predict(face)
+        k = np.argmax(pre)
+        txt = emotion[k] + ' [' + str(int(pre[0,k]*100)) + '%]'
+        cv2.putText(img, txt, (x, y), font, 1.0,(0,255,0),2,cv2.LINE_AA)
+        # cv2.imshow(str(i), face)
+
+    cv2.imshow('Camera', img)
+    if cv2.waitKey(20) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
